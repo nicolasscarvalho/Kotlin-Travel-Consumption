2cdb941107adec05addad0c3ff668e99fee0a802
Nícolas Carvalho <nicolassprofissional@gmail.com>
2024-01-19 09:08:34 -0300

    Initial commit

---
+++ app/src/main/java/com/nicolascarvalho/curso_android_iosi/kotlin_travel_consumption/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nicolascarvalho.curso_android_iosi.kotlin_travel_consumption
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
